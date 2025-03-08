2e55e03a823e04f1b55daba81a6867b1ee4e37bd
Jesus Aguila Dorantes <ja007735@gmail.com>
2025-03-08 14:27:11 -0600

    guardado

---
+++ src/main/java/com/jesus/springboot/di/app/springboot_di/precio.java
@@ -0,0 +1,9 @@
+package com.jesus.springboot.di.app.springboot_di;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:archivo.properties")
+public class precio {
+}
--- src/main/java/com/jesus/springboot/di/app/springboot_di/services/ProductService.java
+++ src/main/java/com/jesus/springboot/di/app/springboot_di/services/ProductService.java
@@ -4,6 +4,7 @@ import com.jesus.springboot.di.app.springboot_di.models.Product;
 import com.jesus.springboot.di.app.springboot_di.repositories.IProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +14,20 @@ import java.util.stream.Collectors;
 @Service
 public class ProductService implements IProductService {
 
+    @Value("${valor.precio}")
+    private Double P;
+
     @Autowired
     @Qualifier("imp2")
     private IProductRepository repository;
 
 
-
     @Override
     public List<Product> findAll(){
+
+
         return repository.findAll().stream().map(p->{
-            Double priceImp=p.getPrice()*1.25d;
+            Double priceImp=p.getPrice()*P;
             //Product newProduct=new Product(p.getId(), p.getName(), priceImp.longValue());
             Product newProduct=(Product)p.clone();
             newProduct.setPrice(priceImp.longValue());
